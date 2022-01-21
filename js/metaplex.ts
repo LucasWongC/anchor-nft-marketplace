281d34d2adc2d3567440e49aad032a7d235d9616
LucasWongC <wm111898@gmail.com>
2022-01-21 09:10:49 -0500

    change marketplace, metaplex

--- js/metaplex.ts
+++ js/metaplex.ts
@@ -1,13 +1,9 @@
-import { Connection, PublicKey } from '@solana/web3.js'
-import { programs } from '@metaplex/js'
-const { Metadata, MetadataData } =
-    programs.metadata
+import { Connection, PublicKey } from "@solana/web3.js";
+import { programs } from "@metaplex/js";
+const { Metadata, MetadataData } = programs.metadata;
 
-export const getMetadata = async (
-    connection: Connection,
-    mint: PublicKey,
-)  => {
-    let metadaPDA = await Metadata.getPDA(mint)
-    const metadataAccount = await connection.getAccountInfo(metadaPDA)
-    return MetadataData.deserialize(metadataAccount.data)
-}
+export const getMetadata = async (connection: Connection, mint: PublicKey) => {
+  let metadaPDA = await Metadata.getPDA(mint);
+  const metadataAccount = await connection.getAccountInfo(metadaPDA);
+  return MetadataData.deserialize(metadataAccount.data);
+};
